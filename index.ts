4475d529d688fd4ec55bb66bfa98e2dab982aa38
nagakirankys <109279733+nagakirankys@users.noreply.github.com>
2024-05-31 11:34:03 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ import * as DOMPurify from 'dompurify';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = 'Typescript Starter';
 
+console.info("Push from Stackbliz");
+
 const testDOMPurify = () => {
   const clean = DOMPurify.sanitize('<b>hello there</b>');
   appDiv.innerHTML = clean;
